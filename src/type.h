7299416244c0e248ab13f29b4a5119480a1844f9
ngaran94 <ngaran94@gmail.com>
2022-06-04 16:43:22 -0500

    precise typedefs

---
+++ src/type.h
@@ -0,0 +1,22 @@
+#ifndef _TYPE_H
+#define _TYPE_H
+
+// typedefs for stdint.h types that are shorter
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+typedef float f32;
+typedef double f64;
+
+#endif
+
